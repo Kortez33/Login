6f58fa6deeb23a55eec65e675fd38989b397f8be
Kelemen Adam <125856024+Kortez33@users.noreply.github.com>
2026-03-31 13:14:35 +0200

    file name fix

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,13 @@
-import { Routes, Route } from 'react-router-dom'; // Ezt importáljuk
+import { Routes, Route } from 'react-router-dom';
 import { useAuthModal } from './contexts/AuthModalContext';
 import Headbar from './components/Headbar';
 import TopSection from './components/TopSection';
 import ValueSection from './components/ValueSection';
 import CardGrid from './components/CardGrid';
 
-// Ide importáld be a Rólunk oldalt, amit korábban csináltunk!
 import AboutPage from './components/AboutPage';
 
-// Létrehozunk egy ideiglenes komponenst a főoldal tartalmának
+
 const HomePage = () => (
   <>
     <TopSection
@@ -27,18 +26,14 @@ function App() {
 
   return (
     <>
-      {/* A Headbar minden oldalon látszani fog, mert a Routes-on KÍVÜL van */}
       <Headbar
         onLoginClick={openLogin}
         onRegisterClick={openRegister}
       />
 
-      {/* Itt dől el, hogy a Headbar alatti részbe mi töltődjön be */}
       <Routes>
-        {/* Főoldal */}
         <Route path="/" element={<HomePage />} />
 
-        {/* Rólunk oldal */}
         <Route path="/rolunk" element={<AboutPage />} />
 
       </Routes>
